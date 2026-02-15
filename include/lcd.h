9bacd0e50d8be4393108fa2bdc498282acad9008
Naga Viswanth <132557170+nagabonthu@users.noreply.github.com>
2026-02-15 17:58:11 +0530

    Refactor function declarations in lcd.h

--- include/lcd.h
+++ include/lcd.h
@@ -1,8 +1,6 @@
 #ifndef LCD_H
 #define LCD_H
 
-#include <lpc21xx.h>
-
 /* LCD pin configuration */
 #define LCD  0x000000FF   // P0.0 - P0.7
 #define RS   (1 << 8)    // P0.8
@@ -10,9 +8,9 @@
 
 /* Function declarations */
 void lcd_init(void);
-void lcd_cmd(unsigned char);
-void lcd_data(unsigned char);
-void lcd_string(unsigned char*);
-void delay_millisec(unsigned int);
+void lcd_cmd(unsigned char cmd);
+void lcd_data(unsigned char data);
+void lcd_string(unsigned char *str);
+void delay_millisec(unsigned int ms);
 
 #endif
